b7846663fc510c3d7212282875993d52c25877e1
luc-caspar <luc.caspar@cross-compass.com>
2025-09-21 19:01:45 +0900

    Implement command line interface and the main training/testing process.

--- main.py
+++ main.py
@@ -1,56 +1,98 @@
 #!/usr/bin/env python3
 from argparse import ArgumentParser
-import numpy as np
+from pathlib import Path
+import torch
 from torch.utils.data import DataLoader
+import numpy as np
 from loguru import logger
 
-from datasets import load_data, get_labels, EmotionDataset, TripletSampler
+from datasets import load_data, EmotionDataset, TripletSampler
 from networks import Contrastive
-from settings import FACED, DEVICE
+from settings import FACED, WIN_SIZE, STRIDE
 
-# TODO: Define command line interface to switch between Train, Test, and Inference
-
-# TODO: load_data
-# TODO: create EmotionDataset
-# TODO: create TripletSampler
-
-# TODO: create network
-# TODO: Load weights from file whenever necessary.
-
-# TODO: Train/test/inference as requested
 
 if __name__ == "__main__":
-    kernel = 5
-    stride = 2
-    batch_size = 32
+    # Declare command line interface
+    parser = ArgumentParser()
+    parser.add_argument('-e', '--epochs', dest='epochs', type=int, default=100,
+                        help='The number of epochs to train/validate the network for.')
+    parser.add_argument('-w', '--weights', dest='weights_file', type=Path, default=None,
+                        help='The relative path to the pre-trained weights.')
+    parser.add_argument('-b', '--batch_size', dest='batch_size', type=int, default=32,
+                        help='The size of a batch to be fed to the network for training/inference.')
+    parser.add_argument('-l', '--l_rate', dest='l_rate', type=float, default=1e-4,
+                        help='The rate of weight adaptation.')
+    parser.add_argument('-d', '--dropout', dest='dropout', type=float, default=0.25,
+                        help='The ratio of neurons to deactivate in a layer during training.')
+    parser.add_argument('-i', '--infer', dest='infer', action='store_true',
+                        help='A flag indicating whether to extract features from the dataset, or train/test the network.')
+    parser.add_argument('-o', '--output', dest='out_file', type=Path, default=None,
+                        help='The relative path to the file in which to write the extracted feature vectors. This is only taken into account when `-i/--infer` is specified.')
+
+    args = parser.parse_args()
 
     # Instantiate model
     logger.info("Loading model")
-    model = Contrastive(in_size=FACED['channels'], hidden_size=FACED['channels'] // 2, out_size=10, l_rate=0.0007, batch_size=batch_size, dropout=0.25)
+    model = Contrastive(in_size=FACED['channels'], hidden_size=FACED['channels'] // 2,
+                        out_size=10, l_rate=args.l_rate, batch_size=args.batch_size, dropout=args.dropout)
+
+    # Load model weight if necessary
+    if args.weights_file is not None:
+        weights_path = args.weights_file.expanduser().resolve()
+        if weights_path.exists() and weights_path.is_file():
+            logger.info(f'Loading weights from: {weights_path}')
+            model.load_state_dict(torch.load(weights_path, weights_only=True))
 
     # Load data
     logger.info("Loading data")
-    data, label_repeat, n_samples, n_segs, n_subs = load_data(kernel, stride)
-    logger.debug(n_samples)
+    data, n_samples, n_segs, n_subs = load_data(WIN_SIZE, STRIDE)
 
-    # Build dataset
-    logger.info("Creating dataset")
-    labels = np.tile(label_repeat, n_subs)
-    data = data.reshape(-1, data.shape[-1])
-    emo_ds = EmotionDataset(data, labels, kernel, stride, n_segs)
 
-    # Instantiate dataloader
-    ts = TripletSampler(n_subs, batch_size, n_samples, get_labels())
-    dl = DataLoader(emo_ds, batch_sampler=ts)
-    logger.debug(f'Length dataloader: {len(dl)}')
+    if args.infer:
+        # Build dataset
+        logger.info("Creating dataset")
+        data = data.reshape(-1, data.shape[-1])
+        emo_ds = EmotionDataset(data, WIN_SIZE, STRIDE, n_segs)
 
-    # Do the testing
-    logger.info("Here we go.")
-    for seq in dl:
-        seq = seq.permute((0, 2, 1)).to(DEVICE)
-        logger.debug(f'Input shape: {seq.shape}')
+        # Do the thing
+        logger.info("Inference - Here we go!")
+        model.infer(emo_ds)
+    else:
+        # Split the subs between train and test sets
+        subs_idx = set(range(n_subs))
 
-        pred = model(seq)
-        logger.debug(f'Output shape: {pred.shape}')
+        test_sub = np.random.choice(list(subs_idx), int(len(subs_idx) * 0.2)).tolist()
+        subs_idx.difference_update(set(test_sub))
 
-        exit()
+        val_sub = np.random.choice(list(subs_idx), int(len(subs_idx) * 0.1)).tolist()
+
+        train_sub = list(subs_idx.difference(set(val_sub)))
+
+        logger.info("Train/Validate - Here we go!")
+        # Build datasets and associated loaders
+        logger.info("Creating datasets")
+        train_ds = EmotionDataset(data[train_sub].reshape(-1, data.shape[-1]),
+                                  WIN_SIZE, STRIDE, n_segs)
+        val_ds = EmotionDataset(data[val_sub].reshape(-1, data.shape[-1]),
+                                WIN_SIZE, STRIDE, n_segs)
+
+        # Instantiate the corresponding loaders
+        train_dl = DataLoader(train_ds,
+                              batch_sampler=TripletSampler(len(train_sub), args.batch_size, n_samples))
+        val_dl = DataLoader(val_ds,
+                            batch_sampler=TripletSampler(len(val_sub), args.batch_size, n_samples))
+
+        # Train and validate
+        model.train_net(train_dl, val_dl, args.epochs)
+
+        logger.info("Test - Here we go!")
+        # Build dataset and associated loader
+        logger.info("Creating datasets")
+        test_ds = EmotionDataset(data[test_sub].reshape(-1, data.shape[-1]),
+                                 WIN_SIZE, STRIDE, n_segs)
+        test_dl = DataLoader(test_ds,
+                             batch_sampler=TripletSampler(len(test_sub), args.batch_size, n_samples),
+                             num_workers=4)
+
+        # Test
+        model.test_net(test_dl)
