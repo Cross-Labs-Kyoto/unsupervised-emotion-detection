21ccc4845d1d7e95aa3fa264fe56a6769ddc3b18
luc-caspar <luc.caspar@cross-compass.com>
2025-09-21 18:53:15 +0900

    Define some constants for the training/inference process.

--- settings.py
+++ settings.py
@@ -3,15 +3,20 @@ from pathlib import Path
 from torch import cuda, device
 
 
+# Declare paths to common folders
 ROOT_DIR = Path(__file__).resolve().parent
 DATA_DIR = ROOT_DIR.joinpath('Data')
 WEIGHT_DIR = ROOT_DIR.joinpath('Weights')
 DEVICE = device('cuda') if cuda.is_available() else device('cpu')
 
+# Faced dataset metadata
 FACED = {'channels': 30,  # Two channels are ignored
          'duration': 30,
          'nb_vids': 28,
          'sample_freq': 250}
 FACED['nb_points'] = FACED['sample_freq'] * FACED['duration']
-
 BANDS = ([4, 7], [8, 13], [14, 30], [31, 50])
+
+# Configure training/inference process
+WIN_SIZE = 5
+STRIDE = 2
