bc6b1c4e0f5889d2ae5fe279ca0e1d27fc9ad160
luc-caspar <luc.caspar@cross-compass.com>
2025-09-24 22:53:38 +0900

    Small configuration that still allow learning, but starts displaying signs of overfitting.

--- main_band.py
+++ main_band.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
                                 out_size=30, l_rate=args.l_rate, batch_size=args.batch_size, dropout=args.dropout)
     else:
         logger.info("Loading model - FC")
-        model = ContrastiveFC(in_size=120, out_size=2, hid_sizes=[2], l_rate=args.l_rate, batch_size=args.batch_size, dropout=args.dropout)
+        model = ContrastiveFC(in_size=120, out_size=3, hid_sizes=[5, 5, 5], l_rate=args.l_rate, batch_size=args.batch_size, dropout=args.dropout)
         WIN_SIZE = 1
         STRIDE = 1
         logger.info(model)
